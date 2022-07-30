557fa0ce5180fe7ce4d38c0facd2a4beff5d5dcc
Elviro Dominguez Soriano <elvirodominguezsoriano@outlook.com>
2022-07-30 13:56:39 -0500

    Basic layouts
    Flexbox and css grid

--- javascript/selectors.js
+++ javascript/selectors.js
@@ -1,5 +1,7 @@
 document.getElementById("myId");
 document.getElementsByClassName("myClass");
 document.getElementsByTagName("div");
+/* always returns a list of elements */
+document.GetElementsByName("myNames");
 /* almost any element can be selected with the following method: */
 document.querySelector("myId/myClass/div");
